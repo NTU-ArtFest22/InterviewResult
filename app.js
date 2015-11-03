d447eed8fc0cd06fd97070558c9754eeb94a3d17
how2945ard <how2945ard@gmail.com>
2015-11-03 17:17:20 +0800

    port -> PORT

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ exports.server = function() {
     app.use(errorhandler());
   }
 
-  app.set('port', process.env.port || 3000);
+  app.set('port', process.env.PORT || 3000);
   app.set('views', path.join(__dirname, 'views'));
   app.set('view engine', 'ejs');
   app.use(morgan('dev'));
